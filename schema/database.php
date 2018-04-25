ff97b20e5884165b364c29fba1348eeb69cb6c9c
v1siond <alexanderpulido81@hotmail.com>
2018-04-25 02:39:25 -0400

    update gitignore

--- schema/database.php
+++ schema/database.php
@@ -5,10 +5,10 @@ Class Schema {
 
 	public function setDb() {
 		$this->db_config = array(
-			'host' => 'localhost',
-			'user' => 'root',
-			'pass' => '',
-			'db_name' => 'zilene',
+			'host' => 'https://www.sileny.com.ve',
+			'user' => 'silenyco_sileny',
+			'pass' => '12345678silenyco',
+			'db_name' => 'silenyco_sileny',
 		);
 		return $this->db_config;
 	}
